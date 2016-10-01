92e2c5e0885ac339a111133daf9129de7faf9ed2
June Rhodes <jrhodes@redpointgames.com.au>
2016-10-01 21:59:44 +1000

    runProcessAndCaptureAndEnv

--- index.js
+++ index.js
@@ -136,6 +136,44 @@ function runProcessAndCapture(cmd, args, callback) {
   });
 }
 
+function runProcessAndCaptureAndEnv(cmd, args, envNew, callback) {
+  var env = {};
+  for (var name in process.env) {
+    if (process.env.hasOwnProperty(name)) {
+      if (process.platform == 'win32') {
+        env[name.toUpperCase()] = process.env[name];
+      } else {
+        env[name] = process.env[name];
+      }
+    }
+  }
+  for (var name in envNew) {
+    if (envNew.hasOwnProperty(name)) {
+      if (process.platform == 'win32') {
+        env[name.toUpperCase()] = envNew[name];
+      } else {
+        env[name] = envNew[name];
+      }
+    }
+  }
+  var child = spawn(
+    cmd,
+    args,
+    { shell: true, env: env, stdio: ['ignore', 'pipe', process.stderr] }
+  );
+  var buffer = "";
+  child.stdout.on('data', (data) => {
+    buffer += data;
+  });
+  child.on('close', (code) => {
+    if (code != 0) {
+      callback(buffer, new Error('Process exited with non-zero exit code.'));
+    } else {
+      callback(buffer)
+    }
+  });
+}
+
 function runProcessAndCaptureAndWorkingDirectory(cmd, args, cwd, callback) {
   var child = spawn(
     cmd,
@@ -162,5 +200,6 @@ module.exports = {
   runProcessWithOutputAndEnv: runProcessWithOutputAndEnv,
   runProcessWithOutputAndEnvAndInput: runProcessWithOutputAndEnvAndInput,
   runProcessAndCapture: runProcessAndCapture,
+  runProcessAndCaptureAndEnv: runProcessAndCaptureAndEnv,
   runProcessAndCaptureAndWorkingDirectory: runProcessAndCaptureAndWorkingDirectory,
 };
